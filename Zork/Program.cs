f231eb25462b26e8528138e5ea8df39a94e0e4d1
abwinstead <54908577+abwinstead@users.noreply.github.com>
2019-09-25 14:55:44 -0400

    5 - The Game World

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Zork
 {
     class Program
     {
+        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static int i = 1;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -11,10 +15,11 @@ namespace Zork
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
-                Console.Write("> ");
+                Console.Write(Rooms[i]);
+                Console.Write(">");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
+                string outputString = "";
                 switch (command)
                 {
                     case Commands.QUIT:
@@ -24,11 +29,40 @@ namespace Zork
                     case Commands.LOOK:
                         outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
                         break;
+
                     case Commands.NORTH:
+                        if (Move(command) == false)
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        break;
                     case Commands.SOUTH:
+                        if (Move(command) == false)
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        break;
                     case Commands.EAST:
+                        if (i + 1 < Rooms.Length)
+                        {
+                            outputString = $"You moved {command}.";
+                            i++;
+                        }
+                        else
+                        {
+                            outputString = $"The way is shut!";
+                        }
+                        break;
                     case Commands.WEST:
-                        outputString = $"You moved {command}.";
+                        if (i - 1 >= 0)
+                        {
+                            outputString = $"You moved {command}.";
+                            i--;
+                        }
+                        else
+                        {
+                            outputString = $"The way is shut!";
+                        }
                         break;
 
                     default:
@@ -41,7 +75,19 @@ namespace Zork
         }
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-        
+
+        private static bool Move(Commands command)
+        {
+            bool isValid = true;
+
+            if (command == Commands.NORTH || command == Commands.SOUTH)
+            {
+                isValid = false;
+            }
+
+            return isValid;
+
+        }
+
     }
-    
 }
