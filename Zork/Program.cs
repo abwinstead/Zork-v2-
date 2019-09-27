3964df2998e5416e227fee6e18aeb0bf7431b750
abwinstead <54908577+abwinstead@users.noreply.github.com>
2019-09-27 18:55:31 -0400

    10 - File Processing (Final)
    because i spent 20 minutes not realizing i had static program twice

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -25,10 +25,13 @@ namespace Zork
                 roomMap[room.Name] = room;
             }
         }
+
         static void Main(string[] args)
         {
+            const string defaultRoomsFilename = "Rooms.txt";
+            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);                //allow the user to specify a filename to read from with code
+
             Console.WriteLine("Welcome to Zork!");
-            string roomsFilename = "Rooms.txt";
             InitializeRoomDescriptions(roomsFilename);
 
             Room previousRoom = null;
@@ -130,25 +133,16 @@ namespace Zork
                 roomMap[Name].Description = Description;
             }
 
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";                                                                                     
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";           
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";                                                           
-
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";                                                              
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";                                          
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house, there is a small window, which is slightly ajar.";      
-
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";                
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";          
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";                                           
+                                     
         }
 
-    private static readonly Room[,] Rooms =
-    {
+
+        private static readonly Room[,] Rooms =
+         {
             { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
             { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
             { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
-    };
+         };
 
         private static readonly List<Commands> Directions = new List<Commands>
         {
@@ -164,6 +158,12 @@ namespace Zork
         {
             Name = 0,
             Description
+        }
+
+        
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0,
         }
     }
 
